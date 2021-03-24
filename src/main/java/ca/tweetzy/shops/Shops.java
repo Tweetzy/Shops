cef01934a5d15905dae2a0e72f61f19a72248d2b
Kiran Hart <kiran.hart@live.com>
2021-03-24 19:11:32 -0400

    literally everything

---
+++ src/main/java/ca/tweetzy/shops/Shops.java
@@ -0,0 +1,10 @@
+package ca.tweetzy.shops;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: March 24 2021
+ * Time Created: 7:09 p.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public class Shops {
+}
