a114c24fb42701b351a845454ae9cb0a47be2729
Kiran Hart <kiran.hart@live.com>
2021-03-24 21:51:50 -0400

    shop setup - basic

---
+++ src/main/java/ca/tweetzy/shops/shop/Shop.java
@@ -0,0 +1,26 @@
+package ca.tweetzy.shops.shop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: March 24 2021
+ * Time Created: 9:44 p.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+
+@Getter
+@Setter
+public class Shop implements Serializable {
+
+    private String id;
+    private String displayName;
+
+    public Shop(String id) {
+        this.id = id;
+    }
+}
