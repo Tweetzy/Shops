94a3a490a8ff8879509aab6df27579a752e1346f
Kiran Hart <kiran.hart@live.com>
2021-03-25 17:48:34 -0400

    used to store custom items that are added to an inventory

---
+++ src/main/java/ca/tweetzy/shops/custom/CustomGUIItem.java
@@ -0,0 +1,29 @@
+package ca.tweetzy.shops.custom;
+
+import ca.tweetzy.shops.api.ShopAPI;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.inventory.ItemStack;
+
+import java.io.Serializable;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: March 25 2021
+ * Time Created: 5:07 p.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+
+@Getter
+@Setter
+public class CustomGUIItem implements Serializable {
+
+    private int slot;
+    private byte[] item;
+
+    public CustomGUIItem(int slot, ItemStack stack) {
+        this.slot = slot;
+        this.item = ShopAPI.getInstance().serializeItemStack(stack);
+    }
+
+}
