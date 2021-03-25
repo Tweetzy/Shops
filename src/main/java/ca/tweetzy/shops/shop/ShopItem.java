1fd1aeda76ae30f11fd94de0bd138d1b8b866684
kiranhart <kiran.hart@live.com>
2021-03-25 04:58:15 +0000

    Shop & ShopItem structure changes

--- src/main/java/ca/tweetzy/shops/shop/ShopItem.java
+++ src/main/java/ca/tweetzy/shops/shop/ShopItem.java
@@ -21,17 +21,31 @@ public class ShopItem implements Serializable {
 
     private String shopId;
     private byte[] item;
+
     private double sellPrice;
     private double buyPrice;
 
-    public ShopItem(String shopId, ItemStack item, double sellPrice, double buyPrice) {
+    private boolean isSellOnly;
+    private boolean isBuyOnly;
+
+    public ShopItem(String shopId, ItemStack item, double sellPrice, double buyPrice, boolean isSellOnly, boolean isBuyOnly) {
         this.shopId = shopId;
         this.item = ShopAPI.getInstance().serializeItemStack(item);
         this.sellPrice = sellPrice;
         this.buyPrice = buyPrice;
+        this.isSellOnly = isSellOnly;
+        this.isBuyOnly = isBuyOnly;
+    }
+
+    public ShopItem(String shopId, ItemStack item, double sellPrice, double buyPrice) {
+        this(shopId, item, sellPrice, buyPrice, false, false);
+    }
+
+    public ShopItem(String shopId, ItemStack item) {
+        this(shopId, item, 1.0D, 1.0D, false, false);
     }
 
     public ShopItem() {
-        this("default", XMaterial.PAPER.parseItem(), 0.0D, 0.0D);
+        this("default", XMaterial.PAPER.parseItem(), 1.0D, 1.0D, false, false);
     }
 }
