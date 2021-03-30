c743c2fc4f1c3a619853e067c5b27a20af551b16
kiranhart <kiran.hart@live.com>
2021-03-30 02:38:07 +0000

    file commit for transfer

---
+++ src/main/java/ca/tweetzy/shops/guis/GUICart.java
@@ -0,0 +1,16 @@
+package ca.tweetzy.shops.guis;
+
+import ca.tweetzy.core.gui.Gui;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: March 29 2021
+ * Time Created: 5:14 p.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public class GUICart extends Gui {
+
+    public GUICart() {
+
+    }
+}
