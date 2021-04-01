7cb5807c29f5227b80864db99f4c816229a71463
Kiran Hart <kiran.hart@live.com>
2021-04-01 17:46:11 -0400

    cart command

---
+++ src/main/java/ca/tweetzy/shops/commands/CommandCart.java
@@ -0,0 +1,49 @@
+package ca.tweetzy.shops.commands;
+
+import ca.tweetzy.core.commands.AbstractCommand;
+import ca.tweetzy.shops.Shops;
+import ca.tweetzy.shops.guis.GUICart;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: April 01 2021
+ * Time Created: 5:40 p.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public class CommandCart extends AbstractCommand {
+
+    public CommandCart() {
+        super(CommandType.PLAYER_ONLY, "cart");
+    }
+
+    @Override
+    protected ReturnType runCommand(CommandSender sender, String... args) {
+        Player player = (Player) sender;
+        Shops.getInstance().getGuiManager().showGUI(player, new GUICart(player));
+        return ReturnType.SUCCESS;
+    }
+
+    @Override
+    public String getPermissionNode() {
+        return "shops.cmd.cart";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "cart";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Open your cart";
+    }
+
+    @Override
+    protected List<String> onTab(CommandSender sender, String... args) {
+        return null;
+    }
+}
