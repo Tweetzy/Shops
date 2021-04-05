b28ae7edb1061a3a313d49898b32987a45e349bb
Kiran Hart <kiran.hart@live.com>
2021-04-04 23:05:48 -0400

    Added the method to send messages to the discord server using webhooks

--- src/main/java/ca/tweetzy/shops/api/ShopAPI.java
+++ src/main/java/ca/tweetzy/shops/api/ShopAPI.java
@@ -2,17 +2,24 @@ package ca.tweetzy.shops.api;
 
 import ca.tweetzy.core.compatibility.ServerVersion;
 import ca.tweetzy.shops.Shops;
+import ca.tweetzy.shops.api.events.ShopBuyEvent;
+import ca.tweetzy.shops.api.events.ShopSellEvent;
 import ca.tweetzy.shops.custom.CustomGUIItemHolder;
+import ca.tweetzy.shops.settings.Settings;
 import ca.tweetzy.shops.shop.Shop;
+import org.apache.commons.lang.WordUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 
+import java.awt.*;
 import java.io.*;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Base64;
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 /**
  * The current file has been created by Kiran Hart
@@ -183,4 +190,64 @@ public class ShopAPI {
         return object;
     }
 
+    public void sendDiscordMessage(String webhook, ShopSellEvent sellEvent, ShopBuyEvent buyEvent) {
+        Objects.requireNonNull(webhook, "Webhook cannot be null when sending discord message");
+
+        DiscordWebhook hook = new DiscordWebhook(webhook);
+        hook.setUsername(Settings.DISCORD_MSG_USERNAME.getString());
+        hook.setAvatarUrl(Settings.DISCORD_MSG_PFP.getString());
+
+        String playerName = sellEvent != null ? sellEvent.getPlayer().getName() : buyEvent.getPlayer().getName();
+        String itemName = "???";
+
+        if (sellEvent != null) {
+            ItemStack deserializedItem = deserializeItem(sellEvent.getShopItem().getItem());
+            itemName = getDiscordFriendlyMessage(deserializedItem);
+        }
+
+        if (buyEvent != null) {
+            if (buyEvent.isFromCart()) {
+                itemName = buyEvent.getCartItems().stream().map(cartItem -> deserializeItem(cartItem.getItem())).map(this::getDiscordFriendlyMessage).collect(Collectors.joining(", "));
+            } else {
+                ItemStack deserializedItem = deserializeItem(buyEvent.getShopItem().getItem());
+                itemName = getDiscordFriendlyMessage(deserializedItem);
+            }
+        }
+
+        int quantity = sellEvent != null ? sellEvent.getQuantity() : buyEvent.getQuantity();
+        double price = sellEvent != null ? sellEvent.getQuantity() * sellEvent.getShopItem().getSellPrice() : buyEvent.isFromCart() ? buyEvent.getCartItems().stream().mapToDouble(item -> item.getQuantity() * item.getBuyPrice()).sum() : buyEvent.getQuantity() * buyEvent.getShopItem().getBuyPrice();
+
+        String[] possibleColours = Settings.DISCORD_MSG_DEFAULT_COLOUR.getString().split("-");
+        Color colour = Settings.DISCORD_MSG_USE_RANDOM_COLOUR.getBoolean()
+                ? Color.getHSBColor(ThreadLocalRandom.current().nextFloat() * 360F, ThreadLocalRandom.current().nextFloat() * 101F, ThreadLocalRandom.current().nextFloat() * 101F)
+                : Color.getHSBColor(Float.parseFloat(possibleColours[0]), Float.parseFloat(possibleColours[1]), Float.parseFloat(possibleColours[2]));
+
+        DiscordWebhook.EmbedObject embedObject = new DiscordWebhook.EmbedObject();
+        embedObject.setTitle(sellEvent != null ? Settings.DISCORD_MSG_SELL_TITLE.getString() : buyEvent.isFromCart() ? Settings.DISCORD_MSG_BUY_CART_TITLE.getString() : Settings.DISCORD_MSG_BUY_TITLE.getString());
+        embedObject.setColor(colour);
+
+        embedObject.addField(Settings.DISCORD_MSG_FIELD_PLAYER_NAME.getString(), Settings.DISCORD_MSG_FIELD_PLAYER_VALUE.getString().replace("%player%", playerName), Settings.DISCORD_MSG_FIELD_PLAYER_INLINE.getBoolean());
+        embedObject.addField(Settings.DISCORD_MSG_FIELD_QTY_NAME.getString(), Settings.DISCORD_MSG_FIELD_QTY_VALUE.getString().replace("%quantity%", String.valueOf(quantity)), Settings.DISCORD_MSG_FIELD_QTY_INLINE.getBoolean());
+        embedObject.addField(Settings.DISCORD_MSG_FIELD_PRICE_NAME.getString(), Settings.DISCORD_MSG_FIELD_PRICE_VALUE.getString().replace("%price%", String.format("%,.2f", price)), Settings.DISCORD_MSG_FIELD_PRICE_INLINE.getBoolean());
+
+        if (sellEvent != null) {
+            embedObject.addField(Settings.DISCORD_MSG_FIELD_ITEM_NAME.getString(), Settings.DISCORD_MSG_FIELD_ITEM_VALUE.getString().replace("%item_name%", itemName), Settings.DISCORD_MSG_FIELD_ITEM_INLINE.getBoolean());
+        }
+
+        if (buyEvent != null) {
+            embedObject.addField(buyEvent.isFromCart() ? Settings.DISCORD_MSG_FIELD_ITEMS_NAME.getString() : Settings.DISCORD_MSG_FIELD_ITEM_NAME.getString(), buyEvent.isFromCart() ? Settings.DISCORD_MSG_FIELD_ITEMS_VALUE.getString().replace("%purchased_items%", itemName) : Settings.DISCORD_MSG_FIELD_ITEM_VALUE.getString().replace("%item_name%", itemName), buyEvent.isFromCart() ? Settings.DISCORD_MSG_FIELD_ITEMS_INLINE.getBoolean() : Settings.DISCORD_MSG_FIELD_ITEM_INLINE.getBoolean());
+        }
+
+        hook.addEmbed(embedObject);
+
+        try {
+            hook.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getDiscordFriendlyMessage(ItemStack stack) {
+        return stack.hasItemMeta() ? stack.getItemMeta().hasDisplayName() ? ChatColor.stripColor(stack.getItemMeta().getDisplayName()) : WordUtils.capitalize(stack.getType().name().toLowerCase().replace("_", " ")) : WordUtils.capitalize(stack.getType().name().toLowerCase().replace("_", " "));
+    }
 }
