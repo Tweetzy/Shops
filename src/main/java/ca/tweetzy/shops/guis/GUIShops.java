d2f71d07232f3f3af5689514ba2b8a7dd0d9a397
Kiran Hart <kiran.hart@live.com>
2021-12-12 22:13:11 -0500

    2.17.1 - rearrangement improvements

--- src/main/java/ca/tweetzy/shops/guis/GUIShops.java
+++ src/main/java/ca/tweetzy/shops/guis/GUIShops.java
@@ -94,9 +94,15 @@ public class GUIShops extends Gui {
 		for (Shop shop : data) {
 			if (shop.isPublic()) {
 				if (shop.isRequiresPermissionToSee() && this.player.hasPermission(shop.getSeePermission())) {
-					setButton(Settings.GUI_SHOPS_DYNAMIC.getBoolean() ? slot++ : Settings.GUI_SHOPS_AUTO_ARRANGE.getBoolean() ? slot++ : editing ? slot++ : shop.getSlot(), getShopIcon(shop), e -> e.manager.showGUI(e.player, new GUIShopContents(e.player, shop, false, false)));
+					setButton(Settings.GUI_SHOPS_DYNAMIC.getBoolean() ? slot++ : Settings.GUI_SHOPS_AUTO_ARRANGE.getBoolean() ? slot++ : editing ? slot++ : shop.getSlot(), getShopIcon(shop), e -> {
+						if (!this.editing)
+							e.manager.showGUI(e.player, new GUIShopContents(e.player, shop, false, false));
+					});
 				} else {
-					setButton(Settings.GUI_SHOPS_DYNAMIC.getBoolean() ? slot++ : Settings.GUI_SHOPS_AUTO_ARRANGE.getBoolean() ? slot++ : editing ? slot++ : shop.getSlot(), getShopIcon(shop), e -> e.manager.showGUI(e.player, new GUIShopContents(e.player, shop, false, false)));
+					setButton(Settings.GUI_SHOPS_DYNAMIC.getBoolean() ? slot++ : Settings.GUI_SHOPS_AUTO_ARRANGE.getBoolean() ? slot++ : editing ? slot++ : shop.getSlot(), getShopIcon(shop), e -> {
+						if (!this.editing)
+							e.manager.showGUI(e.player, new GUIShopContents(e.player, shop, false, false));
+					});
 				}
 			}
 		}
