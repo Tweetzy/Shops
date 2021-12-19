c8c52b536ab8bdda8a598ba6201a7fecee039a3d
Kiran Hart <kiran.hart@live.com>
2021-12-19 02:51:44 -0500

    shop item quantity type

---
+++ src/main/java/ca/tweetzy/shops/api/enums/ShopItemQuantityType.java
@@ -0,0 +1,13 @@
+package ca.tweetzy.shops.api.enums;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: December 19 2021
+ * Time Created: 2:51 a.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public enum ShopItemQuantityType {
+
+	LIMITED,
+	UNLIMITED;
+}
