c2278a590fe2fe868eb8b8bf8cd544c1a0a43f06
Kiran Hart <kiran.hart@live.com>
2021-12-19 02:41:40 -0500

    currency manager

---
+++ src/main/java/ca/tweetzy/shops/model/CurrencyManager.java
@@ -0,0 +1,10 @@
+package ca.tweetzy.shops.model;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: December 19 2021
+ * Time Created: 2:24 a.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public class CurrencyManager {
+}
