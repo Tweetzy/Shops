eb65776152ca506a0cae22664874e1303539e8f3
Kiran Hart <kiran.hart@live.com>
2021-12-19 02:41:52 -0500

    ICurrency

---
+++ src/main/java/ca/tweetzy/shops/api/interfaces/ICurrency.java
@@ -0,0 +1,60 @@
+package ca.tweetzy.shops.api.interfaces;
+
+import lombok.NonNull;
+import org.bukkit.OfflinePlayer;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: December 19 2021
+ * Time Created: 1:42 a.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public interface ICurrency {
+
+	/**
+	 * Is the currency enabled
+	 *
+	 * @return true if enabled
+	 */
+	boolean isEnabled();
+
+	/**
+	 * Get the name of the currency plugin
+	 *
+	 * @return the owning plugin name
+	 */
+	@NonNull String getPluginName();
+
+	/**
+	 * If the plugin supports multiple currency types
+	 * (ex. UltraEconomy), this will state it
+	 *
+	 * @return the currency name
+	 */
+	String getName();
+
+	/**
+	 * Withdraws money from a player's balance
+	 *
+	 * @param player is the {@link OfflinePlayer}
+	 * @param amount is the amount being withdrawn
+	 */
+	void withdraw(@NonNull final OfflinePlayer player, final double amount);
+
+	/**
+	 * Deposit money into a player's balance
+	 *
+	 * @param player is the {@link OfflinePlayer}
+	 * @param amount is the amount being deposited
+	 */
+	void deposit(@NonNull final OfflinePlayer player, final double amount);
+
+	/**
+	 * Checks if a player has the amount specified
+	 *
+	 * @param player is the {@link OfflinePlayer}
+	 * @param amount is the amount being checked for
+	 * @return true if they have enough
+	 */
+	boolean has(@NonNull final OfflinePlayer player, final double amount);
+}
