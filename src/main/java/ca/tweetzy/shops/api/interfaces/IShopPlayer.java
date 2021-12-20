e6a2e2a342fba9f49eee8397bd6b903e23cf86b0
Kiran Hart <kiran.hart@live.com>
2021-12-19 22:51:10 -0500

    IShopPlayer

---
+++ src/main/java/ca/tweetzy/shops/api/interfaces/IShopPlayer.java
@@ -0,0 +1,10 @@
+package ca.tweetzy.shops.api.interfaces;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: December 19 2021
+ * Time Created: 9:59 p.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public interface IShopPlayer {
+}
