fa9f603b6d7422d41736e3e6269cdad11e98393d
Kiran Hart <kiran.hart@live.com>
2021-12-19 22:51:01 -0500

    shop manager

--- src/main/java/ca/tweetzy/shops/model/ShopManager.java
+++ src/main/java/ca/tweetzy/shops/model/ShopManager.java
@@ -1,5 +1,9 @@
 package ca.tweetzy.shops.model;
 
+import ca.tweetzy.shops.impl.Shop;
+import ca.tweetzy.tweety.collection.StrictMap;
+import lombok.NonNull;
+
 /**
  * The current file has been created by Kiran Hart
  * Date Created: December 19 2021
@@ -7,4 +11,11 @@ package ca.tweetzy.shops.model;
  * Usage of any code found within this class is prohibited unless given explicit permission otherwise
  */
 public class ShopManager {
+
+	private final StrictMap<String, Shop> shops = new StrictMap<>();
+
+	public boolean doesShopExists(@NonNull final String shopId) {
+		return this.shops.containsKey(shopId);
+	}
+
 }
