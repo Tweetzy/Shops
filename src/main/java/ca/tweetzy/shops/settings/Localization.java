b2adc2c801549c20bf1290628ef0bdd75d1b2fdb
Kiran Hart <kiran.hart@live.com>
2021-12-19 22:50:47 -0500

    localization begin

--- src/main/java/ca/tweetzy/shops/settings/Localization.java
+++ src/main/java/ca/tweetzy/shops/settings/Localization.java
@@ -10,6 +10,48 @@ import ca.tweetzy.tweety.settings.SimpleLocalization;
  */
 public final class Localization extends SimpleLocalization {
 
+	public static final class ShopCreation {
+
+		public static String ENTER_ID_TITLE;
+		public static String ENTER_ID_SUBTITLE;
+
+		public static String ENTER_DESC_TITLE;
+		public static String ENTER_DESC_SUBTITLE;
+
+		private static void init() {
+			pathPrefix("Shop Creation");
+
+			ENTER_ID_TITLE = getString("Enter ID.Title");
+			ENTER_ID_SUBTITLE = getString("Enter ID.Subtitle");
+
+			ENTER_DESC_TITLE = getString("Enter Description.Title");
+			ENTER_DESC_SUBTITLE = getString("Enter Description.Subtitle");
+		}
+	}
+
+	public static final class Success {
+
+		public static String SHOP_CREATED;
+
+		private static void init() {
+			pathPrefix("Success");
+			SHOP_CREATED = getString("Shop Created");
+		}
+	}
+
+	public static final class Error {
+
+		public static String INVALID_SHOP_ID;
+		public static String SHOP_ID_TAKEN;
+
+		private static void init() {
+			pathPrefix("Error");
+			INVALID_SHOP_ID = getString("Shop ID Invalid");
+			SHOP_ID_TAKEN = getString("Shop ID Taken");
+		}
+	}
+
+
 	@Override
 	protected int getConfigVersion() {
 		return 1;
