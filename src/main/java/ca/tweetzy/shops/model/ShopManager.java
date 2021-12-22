6b42a08a0b5363cd99fef86ed10ed3af27a5b934
Kiran Hart <kiran.hart@live.com>
2021-12-21 23:05:36 -0500

    shop manager added: add, remove, and create shop methods

--- src/main/java/ca/tweetzy/shops/model/ShopManager.java
+++ src/main/java/ca/tweetzy/shops/model/ShopManager.java
@@ -1,8 +1,19 @@
 package ca.tweetzy.shops.model;
 
+import ca.tweetzy.shops.api.ShopsAPI;
+import ca.tweetzy.shops.api.enums.ShopLayout;
+import ca.tweetzy.shops.api.enums.ShopState;
 import ca.tweetzy.shops.impl.Shop;
+import ca.tweetzy.shops.impl.ShopDisplay;
+import ca.tweetzy.shops.impl.ShopSettings;
+import ca.tweetzy.shops.impl.SmartItem;
+import ca.tweetzy.shops.settings.ShopsData;
+import ca.tweetzy.tweety.collection.StrictList;
 import ca.tweetzy.tweety.collection.StrictMap;
+import ca.tweetzy.tweety.remain.CompMaterial;
 import lombok.NonNull;
+
+import java.util.ArrayList;
 
 /**
  * The current file has been created by Kiran Hart
@@ -18,4 +29,27 @@ public class ShopManager {
 		return this.shops.containsKey(shopId);
 	}
 
+	public void addShop(@NonNull final Shop shop) {
+		this.shops.put(shop.getId(), shop);
+	}
+
+	public void removeShop(@NonNull final String id) {
+		this.shops.removeWeak(id);
+	}
+
+	public void createShop(@NonNull final String id, @NonNull final String desc) {
+		final Shop shop = new Shop(
+				id,
+				new SmartItem(CompMaterial.CHEST.name()),
+				id,
+				desc,
+				ShopsAPI.getCurrency("Vault"),
+				new ShopDisplay(ShopLayout.AUTOMATIC, CompMaterial.BLACK_STAINED_GLASS_PANE, new StrictList<>(), new StrictMap<>(), -1, -1),
+				new ShopSettings(false, ShopState.BUY_AND_SELL, false, false, false),
+				new ArrayList<>()
+		);
+
+		this.addShop(shop);
+		ShopsData.getInstance().save(new ArrayList<>(this.shops.values()));
+	}
 }
