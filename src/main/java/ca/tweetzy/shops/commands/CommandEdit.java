00e0360c020900b11b24334887ae3e49ad52233e
Kiran Hart <kiran.hart@live.com>
2021-12-23 00:29:46 -0500

    import cleanup for edit command

--- src/main/java/ca/tweetzy/shops/commands/CommandEdit.java
+++ src/main/java/ca/tweetzy/shops/commands/CommandEdit.java
@@ -3,6 +3,7 @@ package ca.tweetzy.shops.commands;
 import ca.tweetzy.shops.api.ShopsAPI;
 import ca.tweetzy.shops.api.enums.ShopListType;
 import ca.tweetzy.shops.impl.Shop;
+import ca.tweetzy.shops.menu.MenuCurrencyList;
 import ca.tweetzy.shops.menu.MenuShopEdit;
 import ca.tweetzy.shops.menu.MenuShopList;
 import ca.tweetzy.shops.settings.Localization;
@@ -29,6 +30,7 @@ public final class CommandEdit extends AbstractSubCommand {
 		checkConsole();
 
 		final Player player = getPlayer();
+
 
 		if (args.length == 1) {
 			final Shop shop = ShopsAPI.getShop(args[0]);
