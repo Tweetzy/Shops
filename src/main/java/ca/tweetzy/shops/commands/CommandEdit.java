a420d18b95dd0a41cb69f23bb10b7bbb3cd5114a
Kiran Hart <kiran.hart@live.com>
2021-12-22 22:15:29 -0500

    the edit command can now take a shop id argument to open it's editor directly

--- src/main/java/ca/tweetzy/shops/commands/CommandEdit.java
+++ src/main/java/ca/tweetzy/shops/commands/CommandEdit.java
@@ -1,8 +1,15 @@
 package ca.tweetzy.shops.commands;
 
+import ca.tweetzy.shops.api.ShopsAPI;
 import ca.tweetzy.shops.api.enums.ShopListType;
+import ca.tweetzy.shops.impl.Shop;
+import ca.tweetzy.shops.menu.MenuShopEdit;
 import ca.tweetzy.shops.menu.MenuShopList;
+import ca.tweetzy.shops.settings.Localization;
+import ca.tweetzy.tweety.TabUtil;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 /**
  * The current file has been created by Kiran Hart
@@ -10,7 +17,7 @@ import org.bukkit.entity.Player;
  * Time Created: 9:57 p.m.
  * Usage of any code found within this class is prohibited unless given explicit permission otherwise
  */
-public final class CommandEdit extends AbstractSubCommand{
+public final class CommandEdit extends AbstractSubCommand {
 
 	public CommandEdit() {
 		super("edit");
@@ -22,6 +29,24 @@ public final class CommandEdit extends AbstractSubCommand{
 		checkConsole();
 
 		final Player player = getPlayer();
+
+		if (args.length == 1) {
+			final Shop shop = ShopsAPI.getShop(args[0]);
+			if (shop == null) {
+				returnTell(Localization.Error.INVALID_SHOP_ID.replace("{shop_id}", args[0]));
+			}
+
+			new MenuShopEdit(shop).displayTo(player);
+			return;
+		}
+
 		new MenuShopList(ShopListType.EDIT).displayTo(player);
+	}
+
+	@Override
+	protected List<String> tabComplete() {
+		if (args.length == 1)
+			return TabUtil.complete(args[0], ShopsAPI.getShopIds());
+		return NO_COMPLETE;
 	}
 }
