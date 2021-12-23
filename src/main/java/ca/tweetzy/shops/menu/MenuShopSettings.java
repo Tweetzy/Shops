51a6599767cfc38e6e841391487aa05b8c4555fe
Kiran Hart <kiran.hart@live.com>
2021-12-23 00:29:56 -0500

    create MenuShopSettings

---
+++ src/main/java/ca/tweetzy/shops/menu/MenuShopSettings.java
@@ -0,0 +1,10 @@
+package ca.tweetzy.shops.menu;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: December 22 2021
+ * Time Created: 11:01 p.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public final class MenuShopSettings {
+}
