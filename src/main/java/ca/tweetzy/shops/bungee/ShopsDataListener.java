8dd928b40a7c3658d1c7f7725d781c50ac5f4555
Kiran Hart <kiran.hart@live.com>
2021-12-26 17:36:40 -0500

    bungee stuff preparation

---
+++ src/main/java/ca/tweetzy/shops/bungee/ShopsDataListener.java
@@ -0,0 +1,19 @@
+package ca.tweetzy.shops.bungee;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: December 26 2021
+ * Time Created: 4:42 p.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public final class ShopsDataListener implements PluginMessageListener {
+
+	@Override
+	public void onPluginMessageReceived(@NotNull String s, @NotNull Player player, @NotNull byte[] bytes) {
+
+	}
+}
---
+++ src/main/java/ca/tweetzy/shops/database/ShopsDatabase.java
@@ -0,0 +1,12 @@
+package ca.tweetzy.shops.database;
+
+import ca.tweetzy.tweety.database.SimpleDatabase;
+
+/**
+ * The current file has been created by Kiran Hart
+ * Date Created: December 26 2021
+ * Time Created: 4:44 p.m.
+ * Usage of any code found within this class is prohibited unless given explicit permission otherwise
+ */
+public final class ShopsDatabase extends SimpleDatabase {
+}
