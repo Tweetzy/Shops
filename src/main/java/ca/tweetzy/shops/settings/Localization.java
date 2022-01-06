c79203a979753eb0a2556ed6f3bd321ae6e9d4c2
Kiran Hart <kiran.hart@live.com>
2022-01-05 22:00:23 -0500

    begin specific menu translations, and added not a number message

--- src/main/java/ca/tweetzy/shops/settings/Localization.java
+++ src/main/java/ca/tweetzy/shops/settings/Localization.java
@@ -45,14 +45,29 @@ public final class Localization extends SimpleLocalization {
 
 		public static String INVALID_SHOP_ID;
 		public static String SHOP_ID_TAKEN;
+		public static String NOT_A_NUMBER;
 
 		private static void init() {
 			pathPrefix("Error");
 			INVALID_SHOP_ID = getString("Shop ID Invalid");
 			SHOP_ID_TAKEN = getString("Shop ID Taken");
+			NOT_A_NUMBER = getString("Not A Number");
 		}
 	}
 
+	public static final class Menus {
+
+		public static final class Main {
+
+			public static String TITLE;
+
+			private static void init() {
+				pathPrefix("Menus.Main");
+
+				TITLE = getString("Title");
+			}
+		}
+	}
 
 	@Override
 	protected int getConfigVersion() {
