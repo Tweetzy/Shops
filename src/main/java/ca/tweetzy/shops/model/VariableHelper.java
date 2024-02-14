99a2947758ae926a98d27ca1a9e8675d0436372c
Kiran Hart <kiran.hart@live.com>
2024-02-14 15:21:23 -0500

    📦 move list/string var replacer to own class
    Took 20 seconds

---
+++ src/main/java/ca/tweetzy/shops/model/VariableHelper.java
@@ -0,0 +1,35 @@
+package ca.tweetzy.shops.model;
+
+import lombok.NonNull;
+import lombok.experimental.UtilityClass;
+
+import java.util.List;
+
+@UtilityClass
+public final class VariableHelper {
+
+	public List<String> replaceVariable(@NonNull List<String> originalList, final String variable, Object replacement, boolean removeVariable) {
+		final int varIndex = originalList.indexOf(variable);
+
+		if (varIndex == -1) {
+			return originalList;
+		}
+
+		if (removeVariable) {
+			originalList.remove(varIndex);
+			return originalList;
+		}
+
+		if (replacement instanceof String string) {
+			originalList.set(varIndex, string);
+			originalList.removeIf(line -> line.equalsIgnoreCase(variable));
+		}
+
+		if (replacement instanceof List) {
+			originalList.addAll(varIndex, (List) replacement);
+			originalList.removeIf(line -> line.equalsIgnoreCase(variable));
+		}
+
+		return originalList;
+	}
+}
