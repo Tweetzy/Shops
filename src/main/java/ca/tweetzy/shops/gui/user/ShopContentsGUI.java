662c7f88abb1315ec8bea72c8d4b315413f7eb94
Kiran Hart <kiran.hart@live.com>
2024-02-21 18:43:19 -0500

    🗃️ can now provide search keywords & has prePopulate filters applied
    Took 39 seconds

--- src/main/java/ca/tweetzy/shops/gui/user/ShopContentsGUI.java
+++ src/main/java/ca/tweetzy/shops/gui/user/ShopContentsGUI.java
@@ -3,9 +3,13 @@ package ca.tweetzy.shops.gui.user;
 import ca.tweetzy.flight.gui.Gui;
 import ca.tweetzy.flight.gui.events.GuiClickEvent;
 import ca.tweetzy.flight.settings.TranslationManager;
+import ca.tweetzy.flight.utils.ChatUtil;
 import ca.tweetzy.flight.utils.QuickItem;
 import ca.tweetzy.flight.utils.input.TitleInput;
 import ca.tweetzy.shops.Shops;
+import ca.tweetzy.shops.api.cart.Cart;
+import ca.tweetzy.shops.api.filter.FilterOrder;
+import ca.tweetzy.shops.api.filter.FilterType;
 import ca.tweetzy.shops.api.shop.Shop;
 import ca.tweetzy.shops.api.shop.ShopContent;
 import ca.tweetzy.shops.api.shop.ShopContentDisplayType;
@@ -17,16 +21,43 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class ShopContentsGUI extends ShopsPagedGUI<ShopContent> {
 
 	private final Shop shop;
+	private final Cart cart;
+	private String search;
+	private FilterOrder filterOrder;
+	private FilterType filterType;
 
-	public ShopContentsGUI(Gui parent, @NonNull Player player, @NonNull final Shop shop) {
+	public ShopContentsGUI(Gui parent, @NonNull Player player, @NonNull final Shop shop, final String search) {
 		super(parent, player, shop.getDisplayName(), Math.max(2, shop.getShopOptions().getShopDisplay().getRows()), shop.getContent());
 		this.shop = shop;
+		this.cart = Shops.getCartManager().getOrAdd(player);
+		this.filterType = FilterType.NAME;
+		this.filterOrder = FilterOrder.ASCENDING;
+		this.search = search;
 		draw();
+	}
+
+	public ShopContentsGUI(Gui parent, @NonNull Player player, @NonNull final Shop shop) {
+		this(parent, player, shop, null);
+	}
+
+	@Override
+	protected void prePopulate() {
+		if (this.search != null)
+			this.items = shop.getContent().stream().filter(shopContent -> shopContent.isMatch(this.search)).collect(Collectors.toList());
+
+		if (this.filterType == FilterType.NAME)
+			this.items = shop.getContent().stream().sorted(Comparator.comparing(ShopContent::getName)).collect(Collectors.toList());
+		else if (this.filterType == FilterType.PRICE)
+			this.items = shop.getContent().stream().sorted(Comparator.comparing(ShopContent::getBuyPrice)).collect(Collectors.toList());
+		else
+			this.items = shop.getContent().stream().sorted(Comparator.comparing(ShopContent::getMinimumPurchaseQty)).collect(Collectors.toList());
 	}
 
 	@Override
@@ -34,7 +65,7 @@ public final class ShopContentsGUI extends ShopsPagedGUI<ShopContent> {
 		applyBackExit();
 
 		// decorations
-		this.shop.getShopOptions().getShopDisplay().getDecoration().forEach(this::setItem);
+		this.shop.getShopOptions().getShopDisplay().getDecoration().forEach((slot, item) -> setItem(slot, QuickItem.bg(item)));
 
 		// search
 		setButton(this.shop.getShopOptions().getShopDisplay().getSearchButtonSlot(), QuickItem
@@ -50,21 +81,45 @@ public final class ShopContentsGUI extends ShopsPagedGUI<ShopContent> {
 
 			@Override
 			public boolean onResult(String string) {
-//				click.manager.showGUI(click.player, new MarketSearchGUI(MarketCategoryViewGUI.this, click.player, MarketCategoryViewGUI.this.category, string));
+				click.manager.showGUI(click.player, new ShopContentsGUI(ShopContentsGUI.this.parent, click.player, ShopContentsGUI.this.shop, string));
 				return true;
 			}
 		});
 
 		// cart
-
+		drawCart();
 		// filter
+		drawFilter();
+	}
+
+	private void drawFilter() {
 		setButton(this.shop.getShopOptions().getShopDisplay().getFilterButtonSlot(), QuickItem
 				.of(Settings.GUI_SHOP_CONTENT_ITEMS_FILTER.getItemStack())
 				.name(TranslationManager.string(this.player, Translations.GUI_SHOP_CONTENTS_ITEMS_FILTER_NAME))
-				.lore(TranslationManager.list(this.player, Translations.GUI_SHOP_CONTENTS_ITEMS_FILTER_LORE))
+				.lore(TranslationManager.list(this.player, Translations.GUI_SHOP_CONTENTS_ITEMS_FILTER_LORE,
+						"shop_filter_type", ChatUtil.capitalizeFully(this.filterType),
+						"shop_filter_order", ChatUtil.capitalizeFully(this.filterOrder)
+				)).make(), click -> {
+
+			if (click.clickType == ClickType.LEFT)
+				this.filterOrder = this.filterOrder.next();
+
+			if (click.clickType == ClickType.RIGHT)
+				this.filterType = this.filterType.next();
+
+			draw();
+		});
+	}
+
+	private void drawCart() {
+		setButton(this.shop.getShopOptions().getShopDisplay().getCartButtonSlot(), QuickItem
+				.of(Settings.GUI_SHOP_CONTENT_ITEMS_CART.getItemStack())
+				.name(TranslationManager.string(this.player, Translations.GUI_SHOP_CONTENTS_ITEMS_CART_NAME))
+				.lore(TranslationManager.list(this.player, Translations.GUI_SHOP_CONTENTS_ITEMS_CART_LORE, "shopping_cart_item_count", this.cart.getItems().size()))
 				.make(), click -> {
 		});
 	}
+
 
 	@Override
 	protected ItemStack makeDisplayItem(ShopContent content) {
@@ -75,6 +130,11 @@ public final class ShopContentsGUI extends ShopsPagedGUI<ShopContent> {
 	protected void onClick(ShopContent content, GuiClickEvent click) {
 		if (click.clickType == ClickType.LEFT)
 			click.manager.showGUI(click.player, new ShopCheckoutGUI(this, player, this.shop, content));
+
+		if (click.clickType == ClickType.RIGHT) {
+			this.cart.addItem(content);
+			drawCart();
+		}
 	}
 
 	@Override
